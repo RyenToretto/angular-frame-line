93928e59c1f6469b33b401736ab231f9f8b26994
RyenToretto <18273727925@163.com>
2020-10-27 00:21:30 +0800

    新增按钮

--- src/app/components/frame-line/frame-line-children/frame-line-item/frame-line-item.component.ts
+++ src/app/components/frame-line/frame-line-children/frame-line-item/frame-line-item.component.ts
@@ -14,8 +14,51 @@ export class FrameLineItemComponent implements OnInit {
 
     @Input() frameLineItem: frameLineItemType;
 
+    addFrameItemPopper: boolean = false;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    toggleAddFrameItemPopper(): void {
+        this.addFrameItemPopper = !this.addFrameItemPopper;
+    }
+
+    // 新并行元素
+    addFrameItemIntoArray(frameLineItem): void {
+        console.log('--addFrameItemIntoArray(frameLineItem)-->');
+        console.log(frameLineItem);
+        if (frameLineItem.frameLineChildren) {
+            frameLineItem.frameLineChildren.push({
+                topFrame: false,
+                title: '存在数组 则新增',
+                mapKey: 'keyNewBingXing' + Date.now(),
+            });
+        } else { // 不存在数组 则创建数组
+            frameLineItem.frameLineChildren = [{
+                topFrame: false,
+                title: '不存在数组 则创建数组',
+                mapKey: 'keyNewBingXing' + Date.now(),
+            }];
+        }
+        this.addFrameItemPopper = false; // 最后关闭 popper
+    }
+
+    // 新 action
+    addFrameItem(frameLineItem): void {
+        console.log('--addFrameItem(frameLineItem)-->');
+        console.log(frameLineItem);
+
+        const newChildren = [{
+            topFrame: false,
+            title: '新 action',
+            mapKey: 'keyNewAction' + Date.now(),
+            frameLineChildren: frameLineItem.frameLineChildren
+        }];
+
+        frameLineItem.frameLineChildren = newChildren;
+
+        this.addFrameItemPopper = false; // 最后关闭 popper
+    }
 
 }
